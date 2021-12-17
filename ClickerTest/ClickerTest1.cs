06c4fbb7c9d33066a6505150605bbba56a5ffe96
gr601_maevl <gr601_maevl@STUDVM-6>
2021-12-17 11:30:28 +0700

    Добавьте файлы проекта.

---
+++ ClickerTest/ClickerTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Clicker;
+
+namespace ClickerTest
+{
+    [TestClass]
+    public class ClickerTest1
+    {
+        [TestMethod]
+        public void ButtomOneUpGrade_1and10and30and02_312()
+        {
+
+            // исходные данные 10 + 10 * (30 + click * 0.2)
+            int click = 1;
+            int post = 10;
+            int post2 = 30;
+            double post3 = 0.2;
+            double expected = 312;
+
+            // получение значения с помощью тестируемого метода
+            double actual = post + post * (post2 + click * post3);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+
+            
+        }
+    }
+}
---
+++ ClickerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClickerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClickerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0756f23-26a9-4dc3-8e32-6dd26134f036")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
